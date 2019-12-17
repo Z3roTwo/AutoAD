87e2ea0889c1b24dd84db1d8db2ce43135bc9c01
Z3roTwo <jerker.ekman@elev.ga.ntig.se>
2019-12-17 09:11:36 +0100

    Gjorde färdigt ta bort användare osv

--- main.py
+++ main.py
@@ -21,11 +21,12 @@ fileloop = True
 while menu:
     option = input('''[1] Skapa Användare
 [2] Ta bort användare
-[3] Stäng programmet
+[3] Rensa Password.txt
+[4] Stäng programmet
 Val: ''')
     if option == "1" or option == "2":
         if option == "1":
-            print("skapa användare")
+            print("Skapa användare")
             while fileloop:
                 userfile = input("Välj .csv fil: ")
                 try:
@@ -33,7 +34,7 @@ Val: ''')
                         readCSV = csv.reader(CSVfile, delimiter=',')
                         names = []
                         surnames = []
-                        passwords = [0]
+                        passwords = []
                         for row in readCSV:
                             name = row[1]
                             surname = row[2]
@@ -48,26 +49,23 @@ Val: ''')
                                 ))
                             passwords.append(password)
                             password = ""
-                        print(len(names))
+                        print(f'Filen innehåller {len(names)}st rader')
                         while amountloop:
-                            arraynumber1 = int(input("Start From Row: "))
-                            arraynumber2 = int(input("Stop at Row: "))
+                            arraynumber1 = int(input("Starta från rad: "))
+                            arraynumber2 = int(input("Stoppa på rad: "))
                             if arraynumber2 > len(names) - 1:
                                 arraynumber2 = len(names) - 1
-                            print("ksjhalsdfh")
-                            print(arraynumber2)
                             amount = arraynumber2 - arraynumber1 + 1
                             if input(f"Detta kommer försöka skapa {amount} användare. Välj 'a' för att fortsätta: ") == "a":
                                 amountloop = False
-
                         while arraynumber1 <= arraynumber2:
                             print(names[arraynumber1], surnames[arraynumber1], passwords[arraynumber1])
-
                             if systemOS == "Windows":
-                                cmd = f'New-ADUser -Name "{names[arraynumber1]} {surnames[arraynumber1]}" -GivenName "{names[arraynumber1]}" -Surname "{surnames[arraynumber1]}" -SamAccountName "{names[arraynumber1]}" -AccountPassword (ConvertTo-SecureString "{passwords[arraynumber1]}" -AsPlainText -force) -passThru -ChangePasswordAtLogon $True -Enabled $True'
+                                cmd = f'New-ADUser -Name "{names[arraynumber1]} {surnames[arraynumber1]}" -GivenName "{names[arraynumber1]}" -Surname "{surnames[arraynumber1]}" -SamAccountName "{names[arraynumber1][0] + "." + surnames[arraynumber1]} " -AccountPassword (ConvertTo-SecureString "{passwords[arraynumber1]}" -AsPlainText -force) -passThru -ChangePasswordAtLogon $True -Enabled $True'
                                 cmd2 = f'echo "{passwords[arraynumber1]}" >> Passwords.txt'
                                 echothepass = subprocess.call(['powershell', cmd2])
                                 returned_value = subprocess.call(['powershell', cmd])
+                                print(cmd)
                                 print("returned_value: ", returned_value)
                             elif systemOS == "Linux":
                                 cmd = f'sudo useradd --password "{passwords[arraynumber1]}" -c "{names[arraynumber1]} {surnames[arraynumber1]}" -m {names[arraynumber1]}.{surnames[arraynumber1]}'
@@ -76,18 +74,62 @@ Val: ''')
                             else:
                                 arraynumber1 = arraynumber2 - 1
                                 print("This OS is not supported please switch to Windows or Ubuntu")
-                            
                             arraynumber1 += 1
                     fileloop = False
-                except:
+                except  Exception as e:
                     print("Filen finns inte, kom ihåg att den är caps känslig och måste sluta med filtillägget.")
+                    print(e)
         else:
-            print("ta bort användare")
-            print("Work in progress")
-
+            while fileloop:
+                userfile = input("Välj .csv fil: ")
+                try:
+                    with open(userfile) as CSVfile:
+                        readCSV = csv.reader(CSVfile, delimiter=',')
+                        names = []
+                        surnames = []
+                        for row in readCSV:
+                            name = row[1]
+                            surname = row[2]
+                            names.append(name)
+                            surnames.append(surname)
+                        while amountloop:
+                            arraynumber1 = int(input("Start From Row: "))
+                            arraynumber2 = int(input("Stop at Row: "))
+                            if arraynumber2 > len(names) - 1:
+                                arraynumber2 = len(names) - 1
+                            amount = arraynumber2 - arraynumber1 + 1
+                            if input(f"Detta kommer ta bort användare #{arraynumber1} till användare #{arraynumber2}. Välj 'a' för att fortsätta: ") == "a":
+                                amountloop = False
+                        while arraynumber1 <= arraynumber2:
+                            print(names[arraynumber1], surnames[arraynumber1])
+                            if systemOS == "Windows":
+                                cmd = f'Remove-ADUser -Identity {names[arraynumber1][0] + "." + surnames[arraynumber1]}'
+                                returned_value = subprocess.call(['powershell', cmd])
+                                print(cmd)
+                                print("returned_value: ", returned_value)
+                            elif systemOS == "Linux":
+                                cmd = f'userdel "{names[arraynumber1]} {surnames[arraynumber1]}"'
+                                returned_value = subprocess.call(cmd, shell=True)
+                                print("returned_value: ", returned_value)
+                            else:
+                                arraynumber1 = arraynumber2 - 1
+                                print("This OS is not supported please switch to Windows or Ubuntu")
+                            arraynumber1 += 1
+                    fileloop = False
+                except Exception as e:
+                    print("Filen finns inte, kom ihåg att den är caps känslig och måste sluta med filtillägget.")
+                    print(e)
     elif option == "3":
         print("Stänger av..")
         time.sleep(0.5)
         menu = False
     else:
         print("Välj ett av de tre alternativen")
+
+    # Reset all needed Variables
+    option = 0
+    arraynumber1 = 0
+    arraynumber2 = 0
+    amount = 0
+    amountloop = True
+    fileloop = True
